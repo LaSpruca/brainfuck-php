89216cf3657bf4df9b6454bf499982b2307e4b7c
Fallstop <jasper@ngita.co.nz>
2020-02-14 19:44:29 +1300

    Update runBrainfuck.php

--- runBrainfuck.php
+++ runBrainfuck.php
@@ -36,5 +36,5 @@ echo "<br>Inputs:<br>";
 print_r($input);
 $memoryArray = array_fill(0,129,0); //Max of 129 memory addresses because yes
 for($i=0;$i<count($code);$i++){
-    echo "Looks like we haven't coded '",$code[i],"' in yet. Possibly because we haven't codded any yet";
+    echo "Looks like we haven't coded '",$code[$i],"' in yet. Possibly because we haven't codded any yet";
 }
